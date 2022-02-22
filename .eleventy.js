8c2174dd4063755cf8bb1a7de0660db59e243495
DanielHansen-IMD <daniel.hansen@confederationcollege.ca>
2022-02-22 12:40:41 -0500

    adding sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.setBrowserSyncConfig({
+		files: './_site/css/**/*.css'
+	});
   eleventyConfig.addPassthroughCopy('img');
 }
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
